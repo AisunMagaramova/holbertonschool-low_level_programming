490b7f36e1c3e12bba8210afe09b673fbac3409c
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-15 19:23:21 +0000

    task5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
-
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 and prints '+' if n > 0
+ *         0 and prints '0' if n == 0
+ *        -1 and prints '-' if n < 0
+ */
 int print_sign(int n)
 {
 	if (n > 0)
