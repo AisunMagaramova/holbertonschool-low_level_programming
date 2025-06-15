8574851221f20876c4b241c230fabb7d805583c9
AisunMagaramova <maharramovaysun1122@gmail.com>
2025-06-15 13:19:00 +0000

    Task4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-
 /**
  * main - Prints the alphabet excluding 'e' and 'q'
  *
  * Return: Always 0
  */
 int main(void)
-{
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'e' && letter != 'q')
-        {
-            putchar(letter);
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+{char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
