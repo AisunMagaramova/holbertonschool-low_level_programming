a0126bd51bc8e51c3b46c417c90e60accb70ce38
AisunMagaramova <maharramovaysun1122@gmail.com>
2025-06-15 11:27:30 +0000

    Task

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Checks if a random number is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {int n;
 	srand(time(0));
