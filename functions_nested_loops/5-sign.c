74942709bcdcab877feaab977917062d6dbeb938
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-15 19:03:35 +0000

    task5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 
 int print_sign(int n) {
     if (n > 0) {
-        printf("+");
+        write(1, "+", 1);
         return 1;
     } else if (n == 0) {
-        printf("0");
+        write(1, "0",1);
         return 0;
     } else {
-        printf("-");
+        write(1, "-", 1);
         return -1;
     }
 }
