edac13e2decf08858c83f3395531bf8d43b9d9ba
AisunMagaramova <maharramovaysun1122@gmail.com>
2025-06-15 14:23:43 +0000

    Task0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{/* Printing each character of "_putchar" */
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	/* Print the new line */
+	putchar('\n');
+	return (0);
+}
