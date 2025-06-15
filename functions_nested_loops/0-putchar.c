4a74a93c0201b084e696d9b7da228f10767f8d96
AisunMagaramova <maharramovaysun1122@gmail.com>
2025-06-15 15:31:30 +0000

    Task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Prints "_putchar" followed by a new line
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * 
+ * Return: Always 0
  */
 int main(void)
-{/* Printing each character of "_putchar" */
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	/* Print the new line */
-	putchar('\n');
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a'); 
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
