8ab7f7fe84bed80a3171534a73cbe69b0bd42184
AisunMagaramova <maharramovaysun1122@gmail.com>
2025-06-15 16:34:57 +0000

    putchar task

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,31 @@
-#include "_putchar.c"
+#include <unistd.h>
+
+int _putchar(char c);
+
 /**
  * main - Entry point
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ * Return: 1 on success, or -1 on error
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
 }
