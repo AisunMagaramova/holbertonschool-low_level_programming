dde6adbb47c8768de806b84209908a50ff2f9c2e
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-19 10:48:56 +0000

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - iki tam ededin qiymeti deyisir
+ * @a: birinci tam edede
+ * @b: ikinci tam edede  integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int*b)
+{
+
+	int temp;
+	temp = *a;
+	*a = *b; 
+	*b = temp;
+}
