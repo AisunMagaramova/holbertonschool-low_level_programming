06d4b787585fdaffbd62b8606763630b521601af
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-19 10:25:33 +0000

    task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
-void reset_to_98(int *n)
 /**
  * reset_to_98 - updates the value pointed to by a pointer to 98
- * @n: pointer to an int to be updated
+ * @n: pointer to an int to update
  *
  * Return: void
  */
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
