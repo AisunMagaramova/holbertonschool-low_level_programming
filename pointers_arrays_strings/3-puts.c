22b5a5cf0996d1d6a35e2222b9e3ddbcf37a0e9f
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-20 13:10:22 +0000

    task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
-
 /**
  * _puts - ekrana soz cixarir
  * @str: unvanini deyir
  */
 void _puts(char *str)
 {
-    while (*str)
+	while (*str)
     {
-        write(1, str, 1);
-        str++;
+	    write(1, str, 1);
+	str++;
     }
     write(1, "\n", 1);
 }
