2858749c6dcc935fca954b068417bf9626d481e4
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-20 13:24:15 +0000

    task3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -6,10 +6,10 @@
  */
 void _puts(char *str)
 {
-    while (*str)
-    {
-        write(1, str, 1);
-        str++;
-    }
-    write(1, "\n", 1);
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
 }
