499f96eaead55f4634ec058c61000d2635ca1c4b
AisunMagaramova <maharramovaaysun11222gmail.com>
2025-06-21 15:31:30 +0000

    task7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,20 +6,20 @@
  */
 char *leet(char *str)
 {
-	int i, j;
-	char letters[] = "aAeEoOtTlL";
-	char codes[] =   "4433707711";
+    int i, j;
+    char letters[] = "aAeEoOtTlL";
+    char codes[] = "4433077111";
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		for (j = 0; letters[j] != '\0'; j++)
-		{
-			if (str[i] == letters[j])
-			{
-				str[i] = codes[j];
-				break;
-			}
-		}
-	}
-	return (str);
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = 0; letters[j] != '\0'; j++)
+        {
+            if (str[i] == letters[j])
+            {
+                str[i] = codes[j];
+                break;
+            }
+        }
+    }
+    return (str);
 }
