3545596bc7321fdd14e8936e0a965efd6a1b7f98
AisunMagaramova <maharramovaaysun11222gmail.com>
2025-06-21 14:32:07 +0000

    task0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,10 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string to append
+ *
+ * Return: pointer to the resulting string dest
+ */
 char	*_strcat(char *dest, char *src)
 {
 	int	i = 0;
