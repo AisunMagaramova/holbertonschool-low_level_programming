2a37eaabb96a38e0d043df84e762079943864aa6
AisunMagaramova <maharramovaaysun11222gmail.com>
2025-06-21 15:02:54 +0000

    task7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+/**
+ * leet - encodes a string into 1337
+ * @str: input string
+ *
+ * Return: pointer to encoded string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char codes[] = "4433707111";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = codes[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
