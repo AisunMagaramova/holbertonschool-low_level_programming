8546ecf351e5dd634cd7bdf29a355be31a5ba055
AisunMagaramova <maharramovaaysun11222gmail.com>
2025-06-21 13:46:55 +0000

    task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,22 @@
 #include <unistd.h>
-
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be printed
+ */
 void print_rev(char *s)
 {
-    int len = 0;
-    int i;
-
-    /* Find length of the string */
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-
-    /* Print characters in reverse */
-    for (i = len - 1; i >= 0; i--)
-    {
-        write(1, &s[i], 1);
-    }
-
-    /* Print newline */
-    write(1, "\n", 1);
+	int len = 0;
+	int i;
+	/* Find length of the string */
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	/* Print characters in reverse */
+	for (i = len - 1; i >= 0; i--)
+	{
+		write(1, &s[i], 1);
+	}
+	/* Print newline */
+	write(1, "\n", 1);
 }
