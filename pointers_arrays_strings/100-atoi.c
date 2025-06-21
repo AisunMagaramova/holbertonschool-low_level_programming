96292f9249517732acb85dec2f9dbea5ae91dc60
AisunMagaramova <maharramovaaysun11222gmail.com>
2025-06-21 14:14:37 +0000

    task10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,45 @@
 #include <limits.h>
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to convert.
- *
- * Return: The converted integer.
+ * _atoi - convert string to int safely
+ * @s: input string
+ * Return: integer value
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, started = 0;
-	int num = 0;
+	int i = 0, sign = 1;
+	unsigned int num = 0;
+	int found = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
-			; /* ignore '+' */
+			;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			started = 1;
+			found = 1;
 
-			/* Overflow check */
-			if (sign == 1)
+			/* Check overflow before multiplying */
+			if (num > (unsigned int)(INT_MAX) / 10 ||
+			    (num == (unsigned int)(INT_MAX) / 10 && (s[i] - '0') > INT_MAX % 10))
 			{
-				if (num > (INT_MAX - (s[i] - '0')) / 10)
+				if (sign == 1)
 					return (INT_MAX);
-			}
-			else
-			{
-				if (-num < (INT_MIN + (s[i] - '0')) / 10)
+				else
 					return (INT_MIN);
 			}
 
 			num = num * 10 + (s[i] - '0');
 		}
-		else if (started)
+		else if (found)
 			break;
 		i++;
 	}
 
-	return (sign * num);
+	if (sign == -1 && num > (unsigned int)INT_MAX + 1)
+		return (INT_MIN);
+
+	return ((int)(sign * (int)num));
 }
