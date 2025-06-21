ea9821549fc76335d95f98e7281542842fc00486
AisunMagaramova <maharramovaaysun11222gmail.com>
2025-06-21 14:56:16 +0000

    task5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @str: input string
+ *
+ * Return: pointer to the changed string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+
+	return (str);
+}
