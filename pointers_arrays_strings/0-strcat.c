2ab5df8d7c05bec602e96b0e2c1a4f9fbca0bc2c
AisunMagaramova <maharramovaaysun11222gmail.com>
2025-06-21 14:30:46 +0000

    task0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,19 @@
-char *_strcat(char *dest, char *src)
+char	*_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+	int	i = 0;
+	int	j = 0;
 
-    /* dest-in sonuna get */
-    while (dest[i] != '\0')
-        i++;
+	while (dest[i] != '\0')
+		i++;
 
-    /* src-dən simvolları əlavə et */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    /* sonuna null-terminator qoy */
-    dest[i] = '\0';
+	dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
