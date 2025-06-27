387e471057148d41d714973b5b6cc08df6bd312c
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-27 19:42:59 +0000

    task2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h> /* Null ucun lazimdir*/
+/**
+ * _strchr - setrinde ilk 'c' xarakterinin yerini tapir
+ * @s: axtaris aparilacaq setir
+ * @c: tapilacaq xarakter
+ *
+ * Return: 'c' xarakterinin ilk tapildigi yere isare eden painter, tapilmaz null
+ */ 
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	while (s[i] != '\0') /*setrin sonuns qeder get */
+	{
+		if (s[i] ==c) /* eger 'c' tapildisa*/
+			return (&s[i]); /*hemin yere isare et*/
+		i++;
+	}
+	if (c =='\0') /* eger axtarilan null terminatordursa*/
+		return (&s[i]); /* setrin sonundaki '\o'-a isare et*/
+	return(NULL); /*tapilmazsa null qaytar*/
+}
