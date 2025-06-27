76b8e8ae5e955e5ae106abb077b10be4e4af89a6
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-27 19:53:11 +0000

    task2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,18 +5,21 @@
  * @s: axtaris aparilacaq setir
  * @c: tapilacaq xarakter
  *
- * Return: 'c' xarakterinin ilk tapildigi yere isare eden painter, tapilmaz null
- */ 
+ * Return: 'c' xaraktern ilk tapildigi yere isare eden painter, tapilmaz null
+ */
 char *_strchr(char *s, char c)
 {
 	int i = 0;
+
 	while (s[i] != '\0') /*setrin sonuns qeder get */
 	{
-		if (s[i] ==c) /* eger 'c' tapildisa*/
+		if (s[i] == c) /* eger 'c' tapildisa*/
 			return (&s[i]); /*hemin yere isare et*/
 		i++;
 	}
+
 	if (c =='\0') /* eger axtarilan null terminatordursa*/
 		return (&s[i]); /* setrin sonundaki '\o'-a isare et*/
-	return(NULL); /*tapilmazsa null qaytar*/
+
+	return (NULL); /*tapilmazsa null qaytar*/
 }
