73ecfcb4f144572e0263b8d08cf002ceaedb3842
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-27 17:46:12 +0000

    task0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
+#inclide "main.h"
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
-
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
-
-    return s;
+	unsigned int i;//dovr ucun deyisen 
+	 //dovr: 0-dan n-e qeder gedirik ve her bayti doldururq
+	 for (i=0; i<n; i++)
+		       {
+			       s[i]=b;//i-ci bayti 'b' sivolu ile doldur
+		       }
+		       return (s);
 }
