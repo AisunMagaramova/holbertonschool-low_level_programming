8fb329c11eac7f1d33a0e5df2f7540206631f274
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-06-27 18:03:22 +0000

    task0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,20 @@
-#inclide "main.h"
+#include "main.h"
+
+/**
+ * _memset - Bir yaddaş sahəsini sabit bir bayt e  doldurur
+ * @s: Baslangic yaddas unvani ünvan
+ * @b: Doldurulacaq bayt (simvol)
+ * @n: Nece bayt doldurulacaq
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;//dovr ucun deyisen 
-	 //dovr: 0-dan n-e qeder gedirik ve her bayti doldururq
-	 for (i=0; i<n; i++)
-		       {
-			       s[i]=b;//i-ci bayti 'b' sivolu ile doldur
-		       }
-		       return (s);
+    unsigned int i; /* dövr ü� deyisen */
+
+    /* 0-dan n-e qeder gedirik sonra her bayti b ile  doldururuq */
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b; /* i-ci baytı b simvolu ie doldurur */
+    }
+
+    return s; /* yaddasin baslangic unvanini qaytarir */
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,26 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 char *_memset(char *s, char b, unsigned int n);
-char *leet(char *);
-char *cap_string(char *);
-char *string_toupper(char *);
-void reverse_array(int *a, int n);
-int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-int _atoi(char *s);
-char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n);
-void puts2(char *str);
-void puts_half(char *str);
-void rev_string(char *s);
-void print_rev(char *s);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
 
-#endif /* MAIN_H */
+#endif
