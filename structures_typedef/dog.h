10441da97fa8b13142387470c06b4758ae1b4fcb
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-05 10:25:04 +0000

    task 0 dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - it haqq覺nda elumat saxlayan struktur
+ * @name: itin ad覺 (simvol pointeri)
+ * @age: itin yasi
+ * @owner: itin sahibi (simvol pointeri)
+ *
+ * Tesviri: Bu struktur itinesaselumatlar覺n覺 saxlamaq 羹寵羹ur- 
+ * ad覺, ysi ve  sahibinin ad覺 daxil olmaqla.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif 
