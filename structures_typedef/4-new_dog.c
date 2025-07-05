37596cf57bfae104011090479e6ff61f82facd88
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-05 11:08:37 +0000

    task4 dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,58 +1,65 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
-/**
- * new_dog - yeni dog_t yaradır, elumatları kopyalayır
- * @name: itin adı
- * @age: itin yas
- * @owner: itin sahibi
- *
- * Return: yeni yaradılan dog_t pointeri, gursuz olarsa NULL
- */
+int _strlen(char *s)
+{
+    int len = 0;
+    while (s[len] != '\0')
+        len++;
+    return (len);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+    int i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *name_copy, *owner_copy;
+    dog_t *d;
+    char *name_copy, *owner_copy;
 
-	/* Yaddas ayır */
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
+    d = malloc(sizeof(dog_t));
+    if (d == NULL)
+        return NULL;
 
-	/* name üçün yas ve kopyalama */
-	if (name == NULL)
-		name_copy = NULL;
-	else
-	{
-		name_copy = malloc(strlen(name) + 1);
-		if (name_copy == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-		strcpy(name_copy, name);
-	}
+    if (name == NULL)
+        name_copy = NULL;
+    else
+    {
+        name_copy = malloc(_strlen(name) + 1);
+        if (name_copy == NULL)
+        {
+            free(d);
+            return NULL;
+        }
+        _strcpy(name_copy, name);
+    }
 
-	/* owner üçün ys v�e kopyalama */
-	if (owner == NULL)
-		owner_copy = NULL;
-	else
-	{
-		owner_copy = malloc(strlen(owner) + 1);
-		if (owner_copy == NULL)
-		{
-			free(name_copy);
-			free(d);
-			return (NULL);
-		}
-		strcpy(owner_copy, owner);
-	}
+    if (owner == NULL)
+        owner_copy = NULL;
+    else
+    {
+        owner_copy = malloc(_strlen(owner) + 1);
+        if (owner_copy == NULL)
+        {
+            free(name_copy);
+            free(d);
+            return NULL;
+        }
+        _strcpy(owner_copy, owner);
+    }
 
-	/* Deyerleri struktura yesdir */
-	d->name = name_copy;
-	d->age = age;
-	d->owner = owner_copy;
+    d->name = name_copy;
+    d->age = age;
+    d->owner = owner_copy;
 
-	return (d);
+    return d;
 }
