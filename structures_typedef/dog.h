3ab0aba67e312a9b8495e9bca846d2a75386cbba
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-05 10:56:28 +0000

    task3 dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,5 +20,12 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/* Yeni ad  - struct dog üçün do task2 dog_t */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
