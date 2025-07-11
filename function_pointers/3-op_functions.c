34eb13065991a1e228c851c7ffbefca569d273cc
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-11 21:24:12 +0000

    task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,30 +1,49 @@
 #include "3-calc.h"
-/**
- * op_add - Adds two integers
- * @a: First integer
- * @b: Second integer
- * Return: Sum of a and b
- */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
+/**
+ * op_sub - İkiededin ferqini qaytarır
+ * @a: birinci eded
+ * @b: ikinci eded
+ *
+ * Return: ferq(a - b)
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
+/**
+ * op_mul - İkiededin hasilini qaytarır
+ * @a: birinci eded
+ * @b: ikinci eddd
+ *
+ * Return: hasil (a * b)
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
+/**
+ * op_div - İkiededin neini qaytarır
+ * @a: bolunen eded
+ * @b: b�len eded
+ *
+ * Return: nisbet (a / b)
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
+/**
+ * op_mod - İkiededinn qalıq böesini qaytarır
+ * @a: birinci eded
+ * @b: ikinci eded
+ *
+ * Return: qalıq (a % b)
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
