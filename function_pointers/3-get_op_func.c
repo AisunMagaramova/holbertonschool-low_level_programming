a26752b81d1576370ea5ec6d4154452be7b833c3
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-11 21:02:05 +0000

    task3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,23 +1,29 @@
 #include <stddef.h>     /* NULL */
 #include "3-calc.h"
 
+/**
+ * get_op_func - secilmis emeliyyat ucun funksiyya qaytarir
+ * @s:emeliyyat sinvolunu gosteren setir
+ *
+ * Return: uygun funksiya gostericilerii ve  ya NULL egerr tapılmazsa
+ */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op)
-    {
-        if (s[0] == ops[i].op[0] && s[1] == '\0')
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+	while (ops[i].op)
+	{
+		if (s[0] == ops[i].op[0] && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
