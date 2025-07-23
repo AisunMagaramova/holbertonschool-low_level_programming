31cf32c52959a63e22b0edd29bde415a004417aa
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-23 13:30:08 +0000

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - siyahidaki butun elementleri cap edir
+ * @h-gosterilen siyahinin baslangici
+ * Return: siyahidaki node-larin sayi
+ */
+size_t print_list(const list_t *h){
+	size_t count = 0;
+	while (h != NULL)
+	{
+		if(h-> ++ NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", n->len, h->str);
+		h = h-> next;
+		count++;
+	}
+	return count;
+}
