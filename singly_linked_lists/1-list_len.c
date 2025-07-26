9ae283f23784c6eaf1fa04e9990c2cb7afbc1c63
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-26 10:46:37 +0000

    task1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,19 +1,18 @@
 #include <stddef.h>
 #include "lists.h"
 /**
- * list_len: linked listdeki elementlerin sayini sayir
+ * list_len- linked listdeki elementlerin sayini sayir
  * @head: siyahinin ilk node nuna isare eden pointer
  *
- * return: siyahidaki node-larin sayi
+ * Return: siyahidaki node-larin sayi
  */
 size_t list_len(const list_t *head)
 {
 	size_t count = 0;
-	/* linked list boyunca her node ucun dovur edirik*/
 	while (head != NULL)
 	{
-		count++;/*her bir node ucun say artiririq */
-		head = head->next;/* novbeti node kecirik */
+		count++;
+		head = head->next;
 	}
 	return (count);
 }
