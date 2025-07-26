778cb072c2b6fd356acf686e6cb7b70b4ae13628
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-26 10:33:58 +0000

    task1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * list_len: linked list de nece duyun oldugunu sayir
+ */
+size_t list_len(const list_t *head)
+{
+	size_t count = 0;
+	/* linked list boyunca her node ucun dovur edirik*/
+	while (head != NULL)
+	{
+		count++;/*her bir node ucun say artiririq */
+		head = head->next;/* novbeti node kecirik */
+	}
+	return count;
+}
