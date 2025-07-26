c9296f6751ddc2ab43a4a34f8f816e998c61d198
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-26 21:18:00 +0000

    task4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_dlistint - doubly linked listdeki butun nodelari azad edir
+ *  @head: siyahınınaslangicina isare eden pointer
+ *
+ *  Return: hec ne qayitmir voiddi deye 
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
