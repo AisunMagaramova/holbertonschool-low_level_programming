b5969541cc5919064b0e5dc0350a4d3c69f2c52a
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-26 23:49:41 +0000

    task8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  * delete_dnodeint_at_index - dlistint_t linked liste node silir
- *  @head: linked list-in baslangicini gosteren pointer
- *  @index: silinecek node indexi
+ * @head: linked list-in baslangicini gosteren pointer
+ * @index: silinecek node indexi
  *
  *  Return: ugulurdursa 1, ugursuzdusa -1 qaytarire
  */
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (!head || !*head)
 		return (-1);
-	
+
 	while (temp && i < index)
 	 {
 		 temp = temp->next;
@@ -23,17 +23,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	 if (!temp)
 		  return (-1);
+       
+	 if (temp->prev)
+		 temp->prev->next = temp->next;
+	 else
+		 *head = temp->next;
 
-	  if (temp->prev)
-		  temp->prev->next = temp->next;
-	  else
-		  *head = temp->next;
 
-
-	  if (temp->next)
-		  temp->next->prev = temp->prev;
-	  
-	  free(temp);
-
-	   return (1);
+	 if (temp->next)
+		 temp->next->prev = temp->prev;
+	 
+	 free(temp);
+	 
+	 return (1);
 }
