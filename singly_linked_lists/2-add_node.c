d8e3982570523ee0ec97ba4d9fa85218e7698266
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-26 11:22:27 +0000

    task2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * @add_node - siyahinin evveline yeni node elave edir
+ * @head: siyahinin baslangicina isare eden pointerin unvani
+ * @str: elave edilecek setrin sureti
+ *
+ * Return: yeni node-un unvani, yaddas catmazsa NULL
+ *  */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return(NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
