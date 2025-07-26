efbab3de11d8d942b1ae1e341971e9bee9876f67
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-26 12:51:25 +0000

    task2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -40,7 +40,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len =_strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
