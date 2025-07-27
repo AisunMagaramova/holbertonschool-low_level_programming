871e6886266a8bafbebe33226cc786e211420ff1
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-27 09:37:32 +0000

    task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -23,25 +23,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	 {
+	{
 		 close(fd);
 		 return (0);
-	 }
+	}
 
-	  r_bytes = read(fd, buffer, letters);
-	  if (r_bytes == -1)
-	  {
-		  free(buffer);
-		  close(fd);
-		  return (0);
-	  }
+	 r_bytes = read(fd, buffer, letters);
+	 if (r_bytes == -1)
+	 {
+		 free(buffer);
+		 close(fd);
+		 return (0);
+	}
 
-	  w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
-	  free(buffer);
-	  close(fd);
+	w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
+	free(buffer);
+	close(fd);
+	if (w_bytes != r_bytes)
+		return (0);
 
-	  if (w_bytes != r_bytes)
-		  return (0);
-
-	  return (w_bytes);
+	 return (w_bytes);
 }
