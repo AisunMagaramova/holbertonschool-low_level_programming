a9b9be4b5d9e6cc56ad0ab82599879795fe54f8c
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-07-27 10:37:22 +0000

    task3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1);
+	if (fd_to == -1)
 	{
 		close(fd_from);
 		print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
